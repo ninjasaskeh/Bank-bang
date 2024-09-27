398372f5e77773d670afc8811a4893d7c03080d2
ninjasaskeh <77367354+ninjasaskeh@users.noreply.github.com>
2024-09-27 11:18:53 +0700

    Asu Kayang Kamu

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,40 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedin = {
+    firstName: "Arief",
+    lastName: "Maizaki",
+    email: "ariefmaizaki@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedin?.firstName || "Guest"}
+            subtext="Access and manage your account effectively."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedin}
+        tra={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 300.5 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
